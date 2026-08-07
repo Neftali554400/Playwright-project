417bcd4b07939a646939c1516ea798a921a927c5
michael obinali <michael.neftali@gmail.com>
2026-08-07 23:15:11 +0100

    added child window handling

--- tests/ui_controls.spec.js
+++ tests/ui_controls.spec.js
@@ -19,18 +19,38 @@ test('UI controls', async ({ page }) => {
     expect (await page.locator("#terms").isChecked()).toBeFalsy();
     await expect(documentLink).toHaveAttribute("class","blinkingText");
 
-    
+    });
 
     // await page.pause();
 
 
+test.only('child window handling', async ({ browser }) => {
 
-   
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator('[href*="documents-request"]');  
 
-   
+    const [newPage] = await Promise.all([
 
+    context.waitForEvent('page'),
+    documentLink.click(),
+    ])
 
-
-
-
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ") [0]
+    console.log(domain); 
+    await page.locator('#username').type(domain);
+    await page.pause();
+    console.log(await page.locator('#username').textContent());
 });
+   
+
+   
+
+
+
+
+
