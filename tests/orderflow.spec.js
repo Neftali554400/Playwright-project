67b986dcdc2e7c049a32576d7a1b37ea58958593
michael obinali <michael.neftali@gmail.com>
2026-08-13 22:43:05 +0100

    added getbyRoles locators

--- tests/orderflow.spec.js
+++ tests/orderflow.spec.js
@@ -30,13 +30,13 @@ const { test, expect } = require('@playwright/test');
    expect(bool).toBeTruthy();
    await page.locator("text=Checkout").click();
  
-   await page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
+   await page.locator("[placeholder*='Country']").pressSequentially("Nig", { delay: 150 });
    const dropdown = page.locator(".ta-results");
    await dropdown.waitFor();
    const optionsCount = await dropdown.locator("button").count();
    for (let i = 0; i < optionsCount; ++i) {
       const text = await dropdown.locator("button").nth(i).textContent();
-      if (text === " India") {
+      if (text === " Nigeria") {
          await dropdown.locator("button").nth(i).click();
          break;
       }
@@ -52,7 +52,7 @@ const { test, expect } = require('@playwright/test');
    await page.locator("tbody").waitFor();
    const rows = await page.locator("tbody tr");
  
- 
+
    for (let i = 0; i < await rows.count(); ++i) {
       const rowOrderId = await rows.nth(i).locator("th").textContent();
       if (orderId.includes(rowOrderId)) {
